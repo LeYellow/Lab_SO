61160fcdf5d9d2a40d7333907e88d4f044823531
LeYellow <46838036+LeYellow@users.noreply.github.com>
2023-11-13 19:27:16 +0200

    Update probBMP.c - now functional

--- Lab6/probBMP.c
+++ Lab6/probBMP.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <errno.h>
-#include <ctype.h>
-#include <string.h>
 #include <unistd.h>
-#define BUFFER 8
-#define PIC 3
+
 
 int main(int argc, char *argv[])
 {
     int fin,fout;
-    char height[PIC],width[PIC];
-    char buff[BUFFER],output[256];
+    int height=0,width=0;
+    char output[256];
 
+    //verificare argumente
     if(argc!=2)
     {
         perror("invalid number of arguments\n");
     }
 
+    //verificare fisiere
     fin=open(argv[1],O_RDONLY);
     if(fin==-1)
     {
@@ -33,31 +32,15 @@ int main(int argc, char *argv[])
         perror("invalid output file\n");
     }
 
-    /*
-    int=fs;
-    struct stat fileInfo;
-    fs=fstat(fin, &fileInfo);
-    if (fs == -1) {
-        perror("Error getting file information");
-        close(fin);
-        close(fout);
-        exit(-1);
-    }
-    */
-
     //citire "poza"
     lseek(fin,18,SEEK_SET);
-    read(fin,buff,BUFFER);
-
-    printf("\nBuffer: %s\n\n",buff);
-
-    strncpy(width,buff,3);
-    strncpy(height,buff+4,3);
+    read(fin, &width, sizeof(int));
+    read(fin, &height, sizeof(int));
 
     //tiparire detalii in statistica.txt
-    sprintf(output,"Inaltime : %s, Latime : %s",height, width);
+    sprintf(output,"Inaltime : %d, Latime : %d",height, width);
 
-    printf("Inaltime : %s, Latime : %s",height, width);
+    //printf("Inaltime : %d, Latime : %d",height, width);
 
     if (write(fout,output,strlen(output))==-1)
     {
